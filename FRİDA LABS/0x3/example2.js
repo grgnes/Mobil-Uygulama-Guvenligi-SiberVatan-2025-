2d91d425aeb849a8be54ab0f1f492b6110cbaac1
Neslihan <ngurgen08@gmail.com>
2025-08-21 10:25:24 +0300

    lab eklendi, readme güncellendi

--- FRİDA LABS/0x3/example2.js
+++ FRİDA LABS/0x3/example2.js
@@ -1,12 +1,8 @@
 Java.perform(function(){
-    var TextView = Java.use("android.widget.TextView");
-    TextView.setText.overload('java.lang.CharSequence').implementation = function(str) {
-        console.log("[FLAG] " + str);
-        return this.setText(str);
-    };
-
+    
     var Checker = Java.use("com.ad2001.frida0x3.Checker");
     for (var i = 1; i <= 256; i++) {
+        console.log("increase() metodu"+i+ ".kez çağırıldı");
         Checker.increase();
     }
 });
